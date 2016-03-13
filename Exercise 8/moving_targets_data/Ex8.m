ea44331ae935c54500074d7f5d2ca6ded2b55e96
Even Flørenæs <Even@dhcp-10-20-109-56.wlan.ntnu.no>
2016-03-13 15:52:37 +0100

    First attempt on part 6

--- Exercise 8/moving_targets_data/Ex8.m
+++ Exercise 8/moving_targets_data/Ex8.m
@@ -7,7 +7,7 @@
 %   Even Florenes NTNU 2016
 %
 % Last changes:
-%   2016-03-08: First attempt
+%   2016-03-08: First attemptsp
 %
 
 %% PART 2
@@ -165,6 +165,88 @@ conversionValue = (speedSound*frameRate)/(4*pi*demodulationFrequency);
 calculatedVelocity = conversionValue*totalPhaseShift;
 
 figure(9),plot(time(1:630),calculatedVelocity,time,pointVelocity);
+title('Estimated velocity using auto correlation method');
+xlabel('Time [sec]');
+legend('Estimated using the auto correlation method',...
+    'Analytically calculated','Location','southeast');
+
+%% PART 6
+
+load fastmotion.mat
+middleBeamIq = squeeze(iq(:,4,:));
+
+frameRate = s.Framerate_fps; % nFrames/seconds
+
+nFrames = size(middleBeamIq,2); %nFrames
+
+nSamples = size(middleBeamIq,1); 
+
+nSeconds = nFrames/frameRate;   %seconds = (nFrames/(nFrames/seconds))
+
+time = 0:nSeconds/(nFrames-1):nSeconds;
+
+distanceLength = s.iq.DepthIncrementIQ_m;
+
+distance = 0:distanceLength/(nSamples-1):distanceLength;
+
+amplitude = abs(middleBeamIq);
+power = amplitude.^2;
+
+gain = -50;
+dynamicRange = 50;
+
+logarithimicPower = imagelog(power,gain,dynamicRange);
+
+figure,imagesc(time,10^4*distance,logarithimicPower),colormap(gray),...
+    title('M-Mode of IQ-beams'),xlabel('Time[sec]'),ylabel('Distance[cm]');
+hold on
+%% Subpart 1:Measure parameters
+[x,y] = ginput(3);
+
+rotationPeriod = x(2)-x(1); % T [s]
+
+t0 = x(3); 
+
+excenterDistance = (y(3)-mean([y(1),y(2)]))/2; 
+%y(1) and y(2) are supposed to
+%be the same height, but 
+%measurements may misalign.
+%Use mean of the measurements
+%to get best possible precision
+
+fprintf('T= %g sec\n',rotationPeriod);
+fprintf('t0= %g sec\n',t0);
+fprintf('R= %g cm\n',excenterDistance);
+
+%% Subpart 2: Plot position and velocity
+pistonLength = 10;
+fixedPoint = 4;
+
+pistonAngularFrequency = (2*pi*(time-t0))/rotationPeriod;
+pistonPosition = pistonLength+excenterDistance*cos(pistonAngularFrequency);
+pointPosition = fixedPoint+excenterDistance*cos(pistonAngularFrequency);
+
+plot(time,pointPosition,'y'),hold off;
+pistonVelocityAmplitude = -(2*pi*excenterDistance)/rotationPeriod;
+pistonVelocity = pistonVelocityAmplitude*sin(pistonAngularFrequency);
+
+pointVelocity = -pistonVelocity;
+
+figure,plot(time,pointVelocity),title('Point velocity for fastmotion.mat');
+xlabel('Time[sec]');
+
+%% Calculate velocity by auto-correlation method
+firstMiddleBeamIq = middleBeamIq(:,1:nFrames-1);
+secondMiddleBeamIq = middleBeamIq(:,2:nFrames);
+speedSound = 1540;
+demodulationFrequency = s.iq.fDemodIQ_Hz;
+totalPhaseShift = angle(mean(conj(secondMiddleBeamIq).*firstMiddleBeamIq));
+
+conversionValue = (speedSound*frameRate)/(4*pi*demodulationFrequency);
+
+calculatedVelocity = conversionValue*totalPhaseShift;
+
+figure,plot(time(1:432),calculatedVelocity,time,pointVelocity);
 title('Estimated velocity using auto correlation method');
 xlabel('Time [sec]');
 legend('Estimated using the auto correlation method',...
